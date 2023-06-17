6823b79dea3c2880b90ce01a4e474723ff7ad2ae
Charlotte <charlotte.corbett@my.jcu.edu.au>
2023-06-17 21:18:41 +1000

    Commit completed do this now -read and print guitars

--- prac_03/do_this_now_read_and_print_file/read_and_print_cool_guitars.py
+++ prac_03/do_this_now_read_and_print_file/read_and_print_cool_guitars.py
@@ -6,5 +6,5 @@ with open("cool_guitars.txt") as in_file:
         segments = line.strip().split(',')
         name = segments[0]
         year = segments[1]
-        price = segments[2]
+        price = segments[2].strip('\\n')
         print(f"{name} {year} ${price}")
