766ca3e6f3072b091d204ea9e3963ffaaf544b1c
Charlotte <charlotte.corbett@my.jcu.edu.au>
2023-06-17 20:58:22 +1000

    Commit completed do this now -read and print guitars

---
+++ prac_03/do_this_now_read_and_print_file/read_and_print_cool_guitars.py
@@ -0,0 +1,10 @@
+"""Do this now from video 3 in Lecture 3-Files and Exceptions
+"""
+
+with open("cool_guitars.txt") as in_file:
+    for line in in_file:
+        segments = line.strip().split(',')
+        name = segments[0]
+        year = segments[1]
+        price = segments[2]
+        print(f"{name} {year} ${price}")
