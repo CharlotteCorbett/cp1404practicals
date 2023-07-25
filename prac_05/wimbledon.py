3e3c27fbdd2a858c3e3af6fe58523014928f0c53
Charlotte <charlotte.corbett@my.jcu.edu.au>
2023-07-25 19:31:44 +1000

    Commit completed wimbledon.py

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,4 +1,6 @@
 """
+This was my initial code plan:
+
 Pseudocode/Code plan:
 load file into nested list named wimbledon stats
 remove first element (descriptor)
@@ -20,16 +22,26 @@ for champion in wimbledon_stats:
     except KeyError:
         champion_to_wins[champion] = 1
 
+However, as I was implementing this code, I felt that there might be a better way to solve this task then
+what I was currently doing.
+Upon checking the solutions branch for this file, I found that although I was on the right track,
+I was not using constants and my overall program was not neatly organised.
 
+As such, I used the solutions as a guide to figure out where I went wrong and what I can do better.
 """
+FILE = "wimbledon.csv"
+COUNTRY_INDEX = 1
+CHAMPION_INDEX = 2
+
 def main():
-    FILE = "wimbledon.csv"
-    results = load_files(FILE)
-    process_results()
-    display_results()
+    """Read data file and show details of Wimbledon countries and champions"""
+    results = load_files()
+    champion_to_wins, countries = process_results(results)
+    show_results(champion_to_wins, countries)
 
 
-def load_files(FILE):
+def load_files():
+    """Load data from file and into a nested list"""
     results = []
     with open(FILE, "r", encoding="utf-8-sig") as in_file:
         for line in in_file:
@@ -39,13 +51,27 @@ def load_files(FILE):
     return results
 
 
-def process_results():
+def process_results(results):
+    """Process data into set of countries and dictionary of champions"""
+    champion_to_wins = {}
     countries = set()
+    for result in results:
+        countries.add(result[COUNTRY_INDEX])
+        try:
+            champion_to_wins[result[CHAMPION_INDEX]] += 1
+        except KeyError:
+            champion_to_wins[result[CHAMPION_INDEX]] = 1
+    return champion_to_wins, countries
 
-    pass
 
 
-def display_results():
-    pass
+def show_results(champion_to_wins, countries):
+    """Show countries and champions"""
+    print("Wimbledon Champions: ")
+    for champion, wins, in champion_to_wins.items():
+        print(champion, wins)
+    print(f"These {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(country for country in sorted(countries)))
+
 
 main()
