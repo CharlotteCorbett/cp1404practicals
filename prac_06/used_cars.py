a5842e1f63d44510e89e7ca704b56d88955b63d6
Charlotte <charlotte.corbett@my.jcu.edu.au>
2023-07-28 14:34:57 +1000

    Commit completed car.py and used_cars.py

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,13 +9,14 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car(180, 'car')
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
-    my_limo = Car(100)
+    my_limo = Car(100, 'limo')
     my_limo.add_fuel(20)
-    print(f"Car has fuel: {my_limo.fuel}")
+    print(f"{my_limo.name} has fuel: {my_limo.fuel}")
     my_limo.drive(115)
+    print(my_limo)
 main()
